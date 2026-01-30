4f157af79f6bcc51357123566510d0d61e091bfd
Chinnu-2005 <gkrvkoushik@gmail.com>
2026-01-30 14:10:35 +0530

    v1

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "campus_micro_mbility_and_rid_sharing");
+    gtk_header_bar_set_title(header_bar, "CampusGo");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "campus_micro_mbility_and_rid_sharing");
+    gtk_window_set_title(window, "CampusGo");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
